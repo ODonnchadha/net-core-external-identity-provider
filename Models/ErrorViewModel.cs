d36ba673978fac3014ebac7c6fdfc9ffbba0ece4
ODonnchadha <danieldonahue1231@hotmail.com>
2020-06-24 13:22:41 -0500

    Initial creation.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace net_core_external_identity_provider.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
